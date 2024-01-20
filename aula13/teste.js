8cf26ad868b93bfa01c25c6ba4754b347175c043
hqaraujo <leopoldopet726@gmail.com>
2024-01-20 19:32:03 -0300

    novas pastas feitas no final de semana

---
+++ aula13/teste.js
@@ -0,0 +1,12 @@
+var contador = 1 
+while (contador <= 3) { // teste logico no inicio
+    console.log(`Passo ${contador}`)
+    contador++
+}
+
+
+var contador = 1
+do {
+    console.log(`passo ${contador}`)
+    contador++
+} while ( contador <= 6) // teste logico no final
---
+++ aula14/ambiente.js
@@ -0,0 +1,12 @@
+/*
+var c = 1
+whilw (c <= 5) {
+    console.log(c)
+    c++
+}
+*/
+console.log('Vai comeceçar...')
+for (var c = 1;c <= 5;c++) {
+    console.log(c)
+}
+console.log('FIM!')
